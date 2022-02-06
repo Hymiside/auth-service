0bc71f24f36803a24fc8ed25131fa54a3243375a
hymiside <vorochaev2004@icloud.com>
2022-02-07 00:38:36 +0500

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"github.com/Hymiside/auth-microservice/pkg/database"
 	"github.com/Hymiside/auth-microservice/pkg/handler"
 	"github.com/Hymiside/auth-microservice/pkg/server"
+	"github.com/Hymiside/auth-microservice/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	h := new(handler.Handler)
-	srv := new(server.Server)
-	err := srv.RunServer(h.InitRoutes())
-
-	if err != nil {
-		log.Fatal("Сервер упал при запуске", err)
+	if err := initConfig(); err != nil {
+		log.Fatalf("Ошибка инциализации конфига: %s", err.Error())
 	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(server.Server)
+	if err := srv.RunServer(handlers.InitRoutes()); err != nil {
+		log.Fatalf("Сервер упал при запуске: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- dockerfile
+++ dockerfile
@@ -10,6 +10,6 @@ COPY ./ ./
 
 RUN go build cmd/main.go
 
-EXPOSE 8080
+EXPOSE 5000
 
 CMD ["./main"]
